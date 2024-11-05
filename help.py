f95fbd79cbf1787a445f8e4ba81e9b09fe6290ca
Uanra14 <155653954+Uanra14@users.noreply.github.com>
2024-11-05 20:24:31 +0100

    First try

---
+++ help.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def compute_steady_state_capital(beta, delta, alpha):
+    'returns the steady state capital according to equation (2.12) and given production function'
+    return (1/beta - 1 + delta) ** 1/alpha
+
+def make_grid(min, max, n):
+    'returns a grid of n equally spaced points between min and max'
+    return np.linspace(min, max, n)
+
+def get_fasible_range_of_K(current_capital, alpha, delta):
+    'returns the LB and UB of the feasible range of capital values for next period'
+    return {'LB': (1 - delta) * current_capital, 'UB': current_capital ** alpha + (1 - delta) * current_capital}
+
+def get_consumption(current_capital, next_capital, alpha, delta):
+    'returns the consumption given current and next period capital'
+    return current_capital ** alpha + (1 - delta) * current_capital - next_capital
+
+def get_utility(consumption, sigma):
+    'returns the utility of consumption'
+    if (sigma == 1):
+        return np.log(consumption)
+    return (consumption ** (1 - sigma) - 1) / (1 - sigma)
